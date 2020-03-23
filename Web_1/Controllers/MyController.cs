1501a93f5bdc4b95c0e0dbbbf17f7a9306d06b7b
DYCodeGit <62571783+DYCodeGit@users.noreply.github.com>
2020-03-23 23:33:50 +0200

    Create README.md

---
+++ Web_1/Controllers/MyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_1.Controllers
+{
+    public class MyController : Controller
+    {
+        public ActionResult Cat()
+        {
+
+            return View();
+        }
+
+        public ActionResult Dog()
+        {
+
+            return View();
+        }
+
+        [HttpGet()]
+        public ActionResult Welc( string welc = "Привет")
+        {
+            ViewBag.WelcText = welc;
+            return View();
+        }
+        
+
+    }
+}
